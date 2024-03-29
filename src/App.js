5b22c06fa8327c68adfc5738cb45f294d322715e
Bayastan123456 <127396034+Bayastan123456@users.noreply.github.com>
2024-03-29 16:59:24 +0300

    dwdw

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { Provider } from 'react-redux/es/exports'
-import './App.css'
-import Checkout from './TS/Checkout'
-import { store } from './store'
+import "./App.css";
+import Checkout from "./JS/Checkout";
+// import Checkout from './TS/Checkout'
 
 function App() {
-    return (
-        <div className="App">
-            <Provider store={store}>
-                <Checkout />
-            </Provider>
-        </div>
-    )
+  return (
+    <div className="App">
+      <Checkout />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/TS/Checkout.tsx
+++ src/TS/Checkout.tsx
@@ -1,11 +1,6 @@
 import styles from './Checkout.module.css'
 import { LoadingIcon } from './Icons'
-import { useSelector } from 'react-redux'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { AppDispatch, RootState } from '../store'
-import { getProductsTunk } from '../store/product.thunk'
-import { productActions } from '../store/product.Slice'
+
 type Props = {
     id: number
     name: string
@@ -13,8 +8,6 @@ type Props = {
     total: number
     availableCount: number
     orderedQuantity: number
-    decrementHandler: (id: number, price: number) => void
-    incrementHandler: (id: number, price: number) => void
 }
 const Product = ({
     id,
@@ -23,12 +16,7 @@ const Product = ({
     price,
     orderedQuantity,
     total,
-    decrementHandler,
-    incrementHandler,
 }: Props) => {
-    const totalPrice = total.toFixed(2)
-    const enebled = total === 0
-    const disable = availableCount === orderedQuantity
     return (
         <tr>
             <td>{id}</td>
@@ -36,57 +24,23 @@ const Product = ({
             <td>{availableCount}</td>
             <td>${price}</td>
             <td>{orderedQuantity}</td>
-            <td>${totalPrice}</td>
+            <td>${total}</td>
             <td>
-                <button
-                    onClick={() => incrementHandler(id, price)}
-                    className={styles.actionButton}
-                    disabled={disable}
-                >
-                    +
-                </button>
-                <button
-                    onClick={() => decrementHandler(id, price)}
-                    className={styles.actionButton}
-                    disabled={enebled}
-                >
-                    -
-                </button>
+                <button className={styles.actionButton}>+</button>
+                <button className={styles.actionButton}>-</button>
             </td>
         </tr>
     )
 }
 const Checkout = () => {
-    const dispatch = useDispatch<AppDispatch>()
-    useEffect(() => {
-        dispatch(getProductsTunk())
-    }, [])
-    const { products, isLoading, error, totalPrice } = useSelector(
-        (state: RootState) => state.product
-    )
-    const incrementHandler = (id: number, price: number) => {
-        dispatch(productActions.incrementByPrice(price))
-        dispatch(productActions.increment(id))
-    }
-    const decrementHandler = (id: number, price: number) => {
-        dispatch(productActions.decrementByPrice(price))
-        dispatch(productActions.decrement(id))
-    }
-    const price = totalPrice.toFixed(2)
-    let discount = 0
-
-    if (totalPrice > 1000) {
-        discount = totalPrice * 0.1
-    }
-    const count = discount.toFixed(2)
     return (
         <div>
             <header className={styles.header}>
                 <h1>Electro World</h1>
             </header>
             <main>
-                {isLoading && <LoadingIcon />}
-                <h4 style={{ color: 'red' }}>{error}</h4>
+                <LoadingIcon />
+                <h4 style={{ color: 'red' }}>Some thing went wrong</h4>
                 <table className={styles.table}>
                     <thead>
                         <tr>
@@ -100,27 +54,11 @@ const Checkout = () => {
                             <th></th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {products.map((item: any) => {
-                            return (
-                                <Product
-                                    key={item.id}
-                                    id={item.id}
-                                    name={item.name}
-                                    availableCount={item.availableCount}
-                                    price={item.price}
-                                    orderedQuantity={item.quantity}
-                                    total={item.total}
-                                    incrementHandler={incrementHandler}
-                                    decrementHandler={decrementHandler}
-                                />
-                            )
-                        })}
-                    </tbody>
+                    <tbody>{/* Products should be rendered here */}</tbody>
                 </table>
                 <h2>Order summary</h2>
-                <p>Discount: $ {count}</p>
-                <p>Total: $ {price}</p>
+                <p>Discount: $ </p>
+                <p>Total: $ </p>
             </main>
         </div>
     )
